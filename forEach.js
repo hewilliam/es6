606d38cf92a759ba3d7623d7e3d2c422758db4f7
William <he.william@yahoo.com>
2017-12-26 13:48:39 -0800

    added forEach

---
+++ forEach.js
@@ -0,0 +1,40 @@
+var colors = ['red', 'yellow', 'green'];
+
+// for(var i = 0; i < colors.length; i++) {
+//     console.log(colors[i])
+// }
+
+// colors.forEach(function(color){
+//     console.log(color)
+// })
+//////////////////////////////////
+
+// var numbers = [1,2,3,4,5];
+// var sum = 0;
+// function adder(number) {
+//     sum+=number
+// }
+
+// numbers.forEach(adder)
+
+// console.log(sum)
+
+///////////////////////////////////
+
+// var images = [
+//     { height: 10, width: 30 },
+//     { height: 20, width: 90 },
+//     { height: 54, width: 32 }
+//   ];
+//   var areas = [];
+  
+//   images.forEach(function(image){
+//      areas.push(image.height*image.width);
+//   })
+
+///////////////////////////////////
+
+
+
+
+
