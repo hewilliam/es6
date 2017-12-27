5343c4f0afca9a0096986adc28873d8099a75e74
William <he.william@yahoo.com>
2017-12-27 12:54:49 -0800

    added find hof

---
+++ find.js
@@ -0,0 +1,33 @@
+// var Car = function(car) {
+//     this.model = car
+// }
+
+// var cars = [ new Car('honda'),
+// new Car('Lexus'),
+// new Car('asdf')
+// ]
+
+// console.log(cars)
+
+// var filtered = cars.filter(function(car){
+//    return car.model === 'Lexus'
+// })
+
+// console.log(filtered)
+
+////////////////////////////////////////
+// var users = [
+//     { id: 1, admin: false },
+//     { id: 2, admin: false },
+//     { id: 3, admin: true }
+//   ];
+  
+//   var admin;
+  
+//   admin = users.find(function(user){
+//       return user.admin === true
+//   })
+  
+//   admin = users.find((user)=>{
+//       return user.admin===true
+//   })
