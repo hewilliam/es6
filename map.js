1406fc7111f7d67215586ee0c3f0223f33162ff9
William <he.william@yahoo.com>
2017-12-27 12:53:40 -0800

    edited map hof

--- map.js
+++ map.js
@@ -3,14 +3,14 @@
 
 var cars = [
     {
-    car: "honda",
-    price: "cheap"
-}, {
-    car: "BMW",
-    price: "expensive"
-}]
+        car: "honda",
+        price: "cheap"
+    }, {
+        car: "BMW",
+        price: "expensive"
+    }]
 
-var prices = cars.map(function(car) {
+var prices = cars.map(function (car) {
     return car.price
 })
 
@@ -20,22 +20,32 @@ var images = [
     { height: '34px', width: '39px' },
     { height: '54px', width: '19px' },
     { height: '83px', width: '75px' },
-  ];
-  
-  var heights;
-  
-  heights = images.map(function(image){
-      return image.height
-  })
+];
 
-  var trips = [
+var heights;
+
+heights = images.map(function (image) {
+    return image.height
+})
+
+var trips = [
     { distance: 34, time: 10 },
     { distance: 90, time: 50 },
     { distance: 59, time: 25 }
-  ];
-  
-  var speeds;
-  
-  speeds = trips.map(function(trip) {
-      return trip.distance/trip.time
-  })
+];
+
+var speeds;
+
+speeds = trips.map(function (trip) {
+    return trip.distance / trip.time
+})
+
+//Implement a 'pluck' function.  Pluck should accept an array and a string representing a property name and return an  array containing that property from each object. 
+var paints = [{ color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+pluck(paints, 'color'); // returns ['red', 'yellow', 'blue'];
+
+function pluck(array, property) {
+    return array.map(function (element) {
+        return element.color
+    })
+}
